1a18f02e8c92fcca93daf01800152120c03422b8
la-montagne <said-haroun.brahimi@epitech.eu>
2021-10-31 19:50:54 +0100

    create branch

--- src/main.js
+++ src/main.js
@@ -4,4 +4,11 @@ import router from './router'
 import VueChartkick from 'vue-chartkick'
 import 'chartkick/chart.js'
 
-createApp(App).use(VueChartkick).use(router).mount('#app')
+// createApp(App).use(VueChartkick).use(router).mount('#app')
+
+new Vue({
+    router,
+    store,
+    vuetify,
+    render: h => h(App)
+  }).$mount('#app')
